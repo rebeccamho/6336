ec22686c3f4995764d58965b3893e816d5ffb53c
Irene Kuang <ikuang@RLE-EECS-MTL-DHCP-31-229.MIT.EDU>
2018-11-15 14:25:06 -0500

    restructed code for creating p matrix, user should only modify first section of test_run_w_code.m

--- test_run_w_code.m
+++ test_run_w_code.m
@@ -1,61 +1,90 @@
 clear all; close all; clc;
-nLayers = 10;
+%% User-defined parameters for IC (ONLY MODIFY THESE VARIABLES)
+Si = 'Si';
+Bond = 'Bond';
+Cu = 'Cu';
+Gr = 'Gr';
+Air = 'Air';
+
+nLayers = 40;
 nPoints = 40;
-p= zeros(nPoints,nLayers); % A, k/(p*Cp)
-x_start = zeros(nLayers*nPoints,1);
-x_start(:) = 298; %Room temperature to start
-%% Parameters
 
-To = 298; %Room temperature 
+materialLayers = [{Cu} {Gr} {Si}]; % list materials from top to bottom
 
-% Ambient
-kamb = 0.02; %Air thermal cond. [W/m*K]
-dens_air = 1.225; %Air density [kg/m^3]
-hc_air = 1e3; %Air specific heat capacity [J/kg*k]
+thickness = struct; 
+thickness.(Si) = 0.025; 
+% thickness.(Bond) = 0.05;
+thickness.(Cu) = 0.02;
+thickness.(Gr) = 0.005;
 
+chipW = 0.1;
+
+Tstart = 298; %Room temperature 
+
+%% Calculate chip height and make sure the discretization can accomodate for thinnest layer
+nUniqueLayers = length(materialLayers);
+materialThickness = sym(cell2mat(struct2cell(thickness)));
+
+chipH = 0;
+for i = 1:nUniqueLayers
+    chipH = chipH + thickness.(materialLayers{i});
+end
+
+minLayerThickness = chipH / nLayers;
+gcdThickness = double(gcd(materialThickness));
+
+assert(mod(gcdThickness,minLayerThickness) == 0 , 'ERROR: nLayers is not sufficient to discretize IC');
+
+%% Material Parameters
 % Specific heat capacity, units J/(kg*k)
-hcap = ones(nLayers,nPoints);
-hc_Si = 0.7e3; %Silicon
-hc_Bond = 0.68e3; %SiliconDioxide
-hc_Cu = 0.385e3; %Copper
-hc_Gr = 0.7e3; %Graphite specific heat used instead of graphene. 
-hcap(6:10,:) = hc_Si;
-hcap(5,:) = hc_Gr;
-hcap(1:4,:) = hc_Cu;
-% hctest = 0.5e3;
-% hcap(:,:) = hctest;
+hc = struct; 
+hc.(Si) = 0.7e3; 
+hc.(Bond) = 0.68e3;
+hc.(Cu) = 0.385e3;
+hc.(Gr) = 0.7e3; % Graphite specific heat used instead of graphene
+hc.(Air) = 1e3;
 
 % Thermal conductivity , units W/(m*K)
-k = ones(nLayers,nPoints);
-kSi = 155; % silicon
-kBond = 1.38; % bonding and oxide layer
-kCu = 400; % copper interconnects 
-kGr = 5000; % monolayer graphene
-k(6:10,:) = kSi; % bottom
-k(5,:) = kGr;
-k(1:4,:) = kCu; % 1: top
-% ktest = 80;
-% k(:,:) = ktest;
-% k(nLayers-1,:) = 200;
+k = struct;
+k.(Si) = 155; 
+k.(Bond) = 1.38; 
+k.(Cu) = 400; 
+k.(Gr) = 5000; % monolayer graphene
+k.(Air) = 0.02; 
 
 % Density, untis kg/m^3
-dens = ones(nLayers,nPoints);
-dens_Si = 2.329e3; %Silicon
-dens_Bond = 2.65e3; %SiliconDioxide
-dens_Cu = 8.92e3; %Copper
-dens_Gr = 2.267e3; %Graphene
-dens(6:10,:) = dens_Si;
-dens(5,:) = dens_Gr;
-dens(1:4,:) = dens_Cu;
-% dens_test = 2.5e3;
-% dens(:,:) = dens_test;
+dens = struct;
+dens.(Si) = 2.329e3; 
+dens.(Bond) = 2.65e3; 
+dens.(Cu) = 8.92e3; 
+dens.(Gr) = 2.267e3; % monolayer graphene
+dens.(Air) = 1.225; 
 
-% Calculate p matrix.
-p = k./(dens.*hcap); % A, k/(p*Cp)
+% Calculate p values
+pVals = struct; 
+pVals.(Si) = k.(Si)/(dens.(Si)*hc.(Si));
+pVals.(Bond) = k.(Bond)/(dens.(Bond)*hc.(Bond));
+pVals.(Cu) = k.(Cu)/(dens.(Cu)*hc.(Cu));
+pVals.(Gr) = k.(Gr)/(dens.(Gr)*hc.(Gr));
+pVals.(Air) = k.(Air)/(dens.(Air)*hc.(Air));
+
+%% Construct p matrix 
+p = zeros(nLayers,nPoints);
+startLayers = zeros(nUniqueLayers,1); 
+startIndex = 1;
+
+for i = 1:nUniqueLayers
+    m = materialLayers{i};
+    startLayers(i) =  startIndex; 
+    endIndex = startIndex + thickness.(m)/minLayerThickness - 1;
+    p(startIndex:endIndex,:) = pVals.(m); 
+    startIndex = endIndex + 1; 
+end 
+
 %% Construct u vector. 
 Power_diss = 2e5; %Units [W/m^3], Power dissipated per transistor
-Source_Trans = Power_diss/(dens_Si*hc_Si);
-Source_air = To*(kamb/(dens_air*hc_air)); %Units, [W/m^3], heat source for air BC. 
+Source_Trans = Power_diss/(dens.(Si)*hc.(Si));
+Source_air = Tstart*(k.(Air)/(dens.(Air)*hc.(Air))); %Units, [W/m^3], heat source for air BC. 
 %Source_SiO2 = To*(kBond/(dens_Bond*hc_Bond)); %Units, [W/m^3], heat source for SiO2 BC. 
 Source_SiO2 = 0;
 
@@ -66,20 +95,26 @@ u = [Source_Trans, Source_air, Source_air, Source_SiO2];
 %% Use function F
 %p = p/100;
 %u = u/100;
+x_start = zeros(nLayers*nPoints,1);
+x_start(:) = Tstart; %Room temperature to start
 
-[dx_dt,A_mat,U_vec] = F(x_start,u,p);
+otherParams = struct;
+otherParams.chipW = chipW;
+otherParams.chipH = chipH; 
+
+[dx_dt,A_mat,U_vec] = F(x_start,u,p,otherParams);
 
 x_steady=-U_vec\A_mat;
 X_steady = vec2mat(x_steady,nPoints);
-figure(100)
-imagesc(X_steady);
-colorbar;
+% figure(100)
+% imagesc(X_steady);
+% colorbar;
 %% Run Euler script. 
 x_start = zeros(nLayers*nPoints,1);
 x_start(:) = 298; %Room temperature Start
 t_start = 0;
 t_stop = 10;
-timestep = 1; 
+timestep = 0.1; 
 
 
 
@@ -92,11 +127,13 @@ pVisualize.time = t_stop;
 pVisualize.nPoints = nPoints;
 pVisualize.nLayers = nLayers;
 pVisualize.figNum = 10;
+pVisualize.materialLayers = materialLayers;
+pVisualize.startLayers = startLayers;
 
 
 t = t_start:timestep:t_stop;
 fhand = @(x,t)fj2DIC(x,t,A_mat,U_vec);
-freq = 2;
+freq = 5;
 x_trap = trapezoidalNonlinear(x_start,t_start,t_stop,timestep,fhand,freq,pVisualize);
 x_trapFinal = x_trap(:,end);
 
